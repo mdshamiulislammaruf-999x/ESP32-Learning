5c93555b5061d1d3f446ce11a3342fad80435031
Maruf <mdshamiulislammaruf@gmail.com>
2026-06-09 23:24:17 +0600

    First ESP32 LCD project

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include<Arduino.h>
+#include<Wire.h>
+#include<LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C ganja(0x27, 16, 2);
+
+void setup(){
+
+  Wire.begin(21, 22);
+  ganja.init();
+  ganja.backlight();
+
+  ganja.setCursor(0, 0);
+  ganja.print("consuming  ");
+  ganja.setCursor(0, 1);
+  ganja.print("ganjaaaaaa.....");
+
+}
+
+void loop(){
+  //emptyyyyyyyyyyyyyy
+}
